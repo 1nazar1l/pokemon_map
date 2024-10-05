08dc6e12bb52bbe90725bfb7c1e05c2bbc12581e
1nazar1l <116957441+1nazar1l@users.noreply.github.com>
2024-10-05 19:03:03 +0300

    added display of only active pokemon

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
 from .models import Pokemon, PokemonEntity
-
+from django.utils.timezone import localtime
 
 MOSCOW_CENTER = [55.751244, 37.618423]
 DEFAULT_IMAGE_URL = (
@@ -33,14 +33,15 @@ def show_all_pokemons(request):
     pokemon_entities = PokemonEntity.objects.all()
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    print()
     for pokemon_entity in pokemon_entities:
-        add_pokemon(
-            folium_map, 
-            pokemon_entity.lat,
-            pokemon_entity.lon,
-            request.build_absolute_uri(pokemon_entity.pokemon.image.url)
-        )
+        local_time = localtime()
+        if pokemon_entity.appeared_at <= local_time and pokemon_entity.disappeared_at > local_time:
+            add_pokemon(
+                folium_map, 
+                pokemon_entity.lat,
+                pokemon_entity.lon,
+                request.build_absolute_uri(pokemon_entity.pokemon.image.url)
+            )
 
     pokemons_on_page = []
     for pokemon in pokemons:
